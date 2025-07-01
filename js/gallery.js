261812977ed4343191e0cbbe71787ebe704061af
Julieta Fuentes G. <123529373+julieta-fuentes@users.noreply.github.com>
2025-07-01 18:36:41 -0300

    task-completed

--- js/gallery.js
+++ js/gallery.js
@@ -8,3 +8,11 @@ function unDo(){
 	document.getElementById("image").style.backgroundImage = "url('')";
    	document.getElementById("image").innerHTML = "Hover over an image below to display here.";
 }
+
+function addTabfocus(){
+	console.log("It works!");
+	images_array = document.querySelectorAll("img");
+	for (let index = 0; index < images_array.length; index++) {
+		images_array[index].setAttribute("tabindex","0")
+	}
+}
