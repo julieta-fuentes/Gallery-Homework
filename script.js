d98888143744c6b385ed5512ab90b0bbcbc92e5e
Julieta Fuentes G. <123529373+julieta-fuentes@users.noreply.github.com>
2025-07-01 17:56:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+  let backgroundUrl = previewPic.src;
+   document.getElementById("image").style.backgroundImage = "url('" + backgroundUrl + "')";
+   document.getElementById("image").innerHTML = previewPic.alt;
+	}
+
+	function unDo(){
+		document.getElementById("image").style.backgroundImage = "url('')";
+   document.getElementById("image").innerHTML = "Hover over an image below to display here.";
+	}
